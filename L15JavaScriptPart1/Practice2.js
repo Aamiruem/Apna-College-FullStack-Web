9ebb366373c909ab91f49f5235ab87467f78e7cf
Aamir@786 <aamirhussainazad786@gmail.com>
2024-07-09 15:36:48 +0530

    commit add

---
+++ L15JavaScriptPart1/Practice2.js
@@ -0,0 +1,20 @@
+let name = "kamran Hussain";
+
+console.log(typeof(name));
+
+console.log(name.length);
+
+let firstName = "kamran";
+console.log(firstName)
+
+
+
+console.log(firstName[0])
+
+console.log(firstName.length);
+
+console.log(firstName[5]);
+
+
+let z = "kamran " +123;
+console.log(z);
