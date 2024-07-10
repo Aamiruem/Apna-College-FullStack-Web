f2bd940641df4a7eb9fb317497a8f5443c6b6822
Aamir@786 <aamirhussainazad786@gmail.com>
2024-07-10 15:21:26 +0530

    commit add

---
+++ L16javascriptPart2/app.js
@@ -0,0 +1,17 @@
+// console.log("Hello world");
+// console.log("Apna college");
+// let a = 10;
+// let b = 6;
+// let sum = a + b;
+// console.log("sum is: ", a+b);
+
+
+
+
+
+let pencilPrice = 10;
+let erasorPrice = 6;
+let output = `The total price is: ${pencilPrice + erasorPrice} Rupees.`;
+console.log(output);
+
+console.log(`The total price is: ${pencilPrice + erasorPrice} Rupees.`)
