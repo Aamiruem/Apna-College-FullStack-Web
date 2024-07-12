fe2b4a849728f8ab32a8de7bcee518c5e4dc0cfb
Aamir@786 <aamirhussainazad786@gmail.com>
2024-07-12 19:38:31 +0530

    commit add

---
+++ L16javascriptPart2/Arithemetics.js
@@ -0,0 +1,12 @@
+// Arithmetics
+
+let a = 10;
+let b = 5;
+let sum = a + b;
+let difference = a - b;
+let product = a * b;
+let quotient = a / b;
+let remainder = a % b;
+let power = a ** b;
+
+console.log(sum, difference, product, quotient, remainder, power);
