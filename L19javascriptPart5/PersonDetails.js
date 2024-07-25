4ea8d3926f274a9e47b9305343eb5d05f8ca4488
Aamir@786 <aamirhussainazad786@gmail.com>
2024-07-25 12:57:20 +0530

    commit add

---
+++ L19javascriptPart5/PersonDetails.js
@@ -0,0 +1,17 @@
+let person = {
+    name: "John Doe",
+    age: 30,
+    city: "Los Angeles",
+};
+
+console.log("Original person object:");
+console.log(person);
+
+// Edit the city property
+person.city = "New York";
+
+// Add a new property country
+person.country = "United States";
+
+console.log("Updated person object:");
+console.log(person);
---
+++ L19javascriptPart5/carModel.js
@@ -0,0 +1,7 @@
+let car = {
+    name: "Toyota",
+    model: "Corolla",
+    color: "Silver"
+  };
+  
+  console.log("The car's name is " + car.name);
