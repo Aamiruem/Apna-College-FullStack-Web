cc28af001f6d129d53818c0431db070d9c72e067
Aamir@786 <aamirhussainazad786@gmail.com>
2024-07-27 19:11:02 +0530

    commit add

---
+++ L20javascriptPart6/function.js
@@ -0,0 +1,34 @@
+function hello(){
+    console.log('Hello World!');
+}
+
+hello(); // Calling the function
+
+function printName(){
+    console.log('My name is kamran');
+    return 'kamran';
+    console.log('This line will not be executed');
+}
+function print1to5(){
+    for(let i=1; i<=5; i++){
+        console.log(i);
+    }
+}
+print1to5();
+
+
+
+
+
+
+function isAdult(){
+    let age = 18;
+    if(age>=18){
+        console.log("Adult")
+    }
+    else{
+        console.log("Not Adult");
+    }
+}
+
+isAdult();
