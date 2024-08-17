3c05b33d4763b0cc24b1f6590368d3a6d3a1246d
Aamir@786 <aamirhussainazad786@gmail.com>
2024-08-17 17:12:44 +0530

    commit add

--- L25jsMiniProject/apps.js
+++ L25jsMiniProject/apps.js
@@ -1,1 +1,29 @@
-let btn = document.querySelector()
+let btn = document.querySelector("button");
+let ul = document.querySelector("ul");
+let inp = document.querySelector("input");
+
+btn.addEventListener("click", function () {
+    let item = document.createElement("li");
+    // console.log("clicked!");
+    item.innerText = inp.value;
+
+
+    let delBtn = document.createElement("button");
+    delBtn.innerText = "Delete";
+    delBtn.classList.add("delete");
+
+    item.appendChild(delBtn);
+    ul.appendChild(item);
+    inp.value = "";
+});
+
+
+let delBtns = document.querySelectorAll(".delete");
+for(delBtn of delBtns){
+    delBtn.addEventListener("click", function () {
+        delBtn.parentNode.remove();
+        let par = this.parentElement;
+        console.log(par);
+        par.remove();
+    });
+}
