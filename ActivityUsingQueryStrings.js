ac2dfdae5afd2b65a9f01422e346364d1c61cb2e
Aamir@786 <aamirhussainazad786@gmail.com>
2024-08-23 12:24:42 +0530

    commit add

---
+++ ActivityUsingQueryStrings.js
@@ -0,0 +1,33 @@
+let url = "http://universities.hipolabs.com/search?name=";
+let btn = document.querySelector("button");
+btn.addEventListener('click', async () => {
+    let country = document.querySelector("input").value;
+    console.log(country);
+    let colleges = getColleges(country);
+    let colArr = await getColleges(country);
+    // console.log(colArr);
+    show(colArr);
+});
+
+function show(colArr){
+    let list = document.querySelector("list");
+    for(col of colArr){
+        console.log(col.name);
+        let li = document.createElement("li");
+        li.innerText = col.name;
+        list.appendChild(li);
+    }
+}
+
+let country = "nepal";
+async function getColleges(country){
+    try{
+        let res = await axios.get(url+country);
+        // console.log(res.data);
+        return res.data;
+    } catch(e){
+        // console.log("ERROR - ",e);
+        // return "An error occurred while fetching data. Please try again later.";
+        return [];
+    }
+}
