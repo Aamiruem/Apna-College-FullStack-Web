22e5ef9880d4d576a02089e9a54a3ffc4cef230c
Aamir@786 <aamirhussainazad786@gmail.com>
2024-08-28 13:40:37 +0530

    commit add

---
+++ L30NodeJS/Import/math.js
@@ -0,0 +1,11 @@
+
+export const sum = (a, b) => a + b;
+
+export const nul = (a, b) => a * b;
+
+export const g = 9.8;
+export const PI = 3.14;
+
+module.exports = { sum, nul, g, PI, sub};
+
+module.exports = "Hello";
---
+++ L30NodeJS/Import/script.js
@@ -0,0 +1,7 @@
+import { sum, PI } from "./math/.js";
+import { generate } from "random=words";
+// console.log(PI);
+
+// console.log(sum(2, 3));
+
+console.log(generate());
