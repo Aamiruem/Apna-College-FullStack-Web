ac00e829d2380665aae65b825be55c625821ea6f
Aamir@786 <aamirhussainazad786@gmail.com>
2024-09-04 16:16:30 +0530

    commit add

--- L34BackendRest/index.js
+++ L34BackendRest/index.js
@@ -5,9 +5,12 @@ const port = 8080;
 const path = require("path");
 const { v4: uuidv4 } = require('uuid');
 
+const methodoverride = require("method-override");
+
 
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true }));
+app.use(methodoverride("_method"));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
