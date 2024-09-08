507d61e49a563f5b8cb6776ff657ab752a1b2680
Aamir@786 <aamirhussainazad786@gmail.com>
2024-09-08 10:55:38 +0530

    commit add

---
+++ L36BackendNodeWithSQL/index.js
@@ -0,0 +1,22 @@
+// ESM
+// import { faker } from '@faker-js/faker';
+
+// CJS
+const { faker } = require("@faker-js/faker");
+
+let createRandomUser = ()=> {
+    return {
+    userId: faker.string.uuid(),
+    username: faker.internet.userName(),
+    email: faker.internet.email(),
+    avatar: faker.image.avatar(),
+    password: faker.internet.password(),
+    birthdate: faker.date.birthdate(),
+    registeredAt: faker.date.past(),
+    };
+}
+console.log(createRandomUser)
+
+// export const users = faker.helpers.multiple(createRandomUser, {
+//   count: 5,
+// });
---
+++ L36BackendNodeWithSQL/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+export const users = faker.helpers.multiple(createRandomUser, {
+//   count: 5,
+// });
