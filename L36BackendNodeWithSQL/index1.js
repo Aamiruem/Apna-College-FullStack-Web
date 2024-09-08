9c3deb4e3a60450de71733f91e8ba06c2ef6ff98
Aamir@786 <aamirhussainazad786@gmail.com>
2024-09-08 11:38:25 +0530

    commit add

--- L36BackendNodeWithSQL/index1.js
+++ L36BackendNodeWithSQL/index1.js
@@ -1,5 +1,26 @@
 const { faker } = require("@faker-js/faker");
 
+const mysql = require('mysql2');
+
+// create the connection to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'delta_app',
+    password: 'Aamir@786'
+});
+
+try {
+    connection.query("SHOW TABLES", (err, result) => {
+    if (err) throw err;
+    console.log(result);
+});
+} catch (err) {
+    console.log(err);
+}
+connection.end();
+// create the connection to database
+
 let getRandomUser = ()=> {
     return {
     id: faker.string.uuid(),
@@ -8,4 +29,3 @@ let getRandomUser = ()=> {
     password: faker.internet.password(),
     };
 }
-console.log(getRandomUser)
