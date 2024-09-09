111c6bcc83f0684a006e76c82dd7598264b191b9
Aamir@786 <aamirhussainazad786@gmail.com>
2024-09-09 13:45:49 +0530

    commit add

--- L36BackendNodeWithSQL/index.js
+++ L36BackendNodeWithSQL/index.js
@@ -1,22 +1,86 @@
-// ESM
-// import { faker } from '@faker-js/faker';
 
-// CJS
+// const { faker } = require("@faker-js/faker");
+
+// let createRandomUser = ()=> {
+//     return {
+//     userId: faker.string.uuid(),
+//     username: faker.internet.userName(),
+//     email: faker.internet.email(),
+//     avatar: faker.image.avatar(),
+//     password: faker.internet.password(),
+//     birthdate: faker.date.birthdate(),
+//     registeredAt: faker.date.past(),
+//     };
+// }
+// console.log(createRandomUser())
+
+
+
+
+
+
 const { faker } = require("@faker-js/faker");
+const mysql = require('mysql2');
 
-let createRandomUser = ()=> {
-    return {
-    userId: faker.string.uuid(),
-    username: faker.internet.userName(),
-    email: faker.internet.email(),
-    avatar: faker.image.avatar(),
-    password: faker.internet.password(),
-    birthdate: faker.date.birthdate(),
-    registeredAt: faker.date.past(),
-    };
+// create the connection to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'delta_app',
+    password: 'Aamir@786'
+});
+
+
+let q = "INSERT INTO user (id, username, email, password) VALUES ?";
+
+let getRandomUser = ()=> {
+    return [
+    faker.string.uuid(),
+    faker.internet.userName(),
+    faker.internet.email(),
+    faker.internet.password(),
+    ];
 }
-console.log(createRandomUser)
 
-// export const users = faker.helpers.multiple(createRandomUser, {
-//   count: 5,
-// });
+let data = [];
+
+for (let i = 1; i < 100; i++) {
+    data.push(getRandomUser());
+    // console.log(getRandomUser());
+}
+
+
+
+
+// let user = [
+// ["123", "234_newuser", "abc@gmail.come", "abc"],
+// ["1453", "267_newuser", "afdf@gmail.come", "abcd"],
+// ];
+
+
+
+
+try {
+    connection.query(q, [data], (err, result) => {
+    if (err) throw err;
+        console.log(result.length);
+        console.log(result[0]);
+        console.log(result[0].id);
+});
+} catch (err) {
+    console.log(err);
+}
+connection.end();
+
+
+
+
+// let getRandomUser = ()=> {
+//     return [
+//     faker.string.uuid(),
+//     faker.internet.userName(),
+//     faker.internet.email(),
+//     faker.internet.password(),
+//     ];
+// }
+// console.log(getRandomUser());
--- L36BackendNodeWithSQL/schema.sql
+++ L36BackendNodeWithSQL/schema.sql
@@ -1,21 +1,12 @@
-SHOW TABLES;
-
 
 CREATE TABLE user(
-id VARCHAR(50) PRIMARY KEY,
-usename VARCHAR(50) UNIQUE,
-email VARCHAR(50) UNIQUE NOT NULL ,
-password VARCHAR(50) NOT NULL
-);
-
-
-
-CREATE TABLE users (
-    id VARCHAR(50) PRIMARY KEY,
+    id INT PRIMARY KEY,
     username VARCHAR(50) UNIQUE,
     email VARCHAR(50) UNIQUE NOT NULL,
-    password VARCHAR(50) NOT NULL
+    password VARCHAR(30) NOT NULL
 );
 
-INSERT INTO users (id, username, email, password) VALUES (?, ?, ?, ?);
-SELECT * FROM users WHERE email = ? AND password = ?;
+CREATE TABLE posts(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    title VARCHAR(30) NOT NULL,
