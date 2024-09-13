f428319be6154ab260f5cb76de35067ff08d5a9b
Aamir@786 <aamirhussainazad786@gmail.com>
2024-09-13 12:06:30 +0530

    commit add

---
+++ L39BackendNodeJsPart1/import/script.js
@@ -0,0 +1,25 @@
+// import { sum, nul, g, PI, sub } from "./math.js";
+// import randomWords from "random-words";
+
+
+// import { generate } from "random=words";
+// // console.log(PI);
+
+// // console.log(sum(2, 3));
+
+// console.log(generate());
+
+
+
+
+import randomWords from 'random-words';
+
+// Example usage
+const words = randomWords();
+console.log(words);
+
+const randomWords = require('random-words');
+
+// Example usage
+const word = randomWords();
+console.log(word);
