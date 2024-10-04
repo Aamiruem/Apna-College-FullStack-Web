da82a35c1538ea298de6945cfbdd8e99ffeebc5f
Aamir Hussain <aamirhussainazad786@gmail.com>
2024-10-04 11:46:45 +0530

    commit add

--- L45BackendNodewithSQL/index.js
+++ L45BackendNodewithSQL/index.js
@@ -1,6 +1,10 @@
 const { faker } = require('@faker-js/faker');
 const mysql = require('mysql2');
 
+const express = require('express');
+const app = express();
+
+
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -8,13 +12,13 @@ const connection = mysql.createConnection({
     password: 'Aamirazad@786'
 });
 
-connection.connect((err) => {
-    if (err) {
-        console.error('Error connecting to the database:', err);
-        return;
-    }
-    console.log('Connected to the database');
-});
+// connection.connect((err) => {
+//     if (err) {
+//         console.error('Error connecting to the database:', err);
+//         return;
+//     }
+//     console.log('Connected to the database');
+// });
 
 
 // CREATE TABLE user(
@@ -25,39 +29,77 @@ connection.connect((err) => {
 // );
 
 
-// let q = "SHOW TABLES";
-let q = "INSERT INTO user (id, username, email, password) VALUES ?";
-
-let users = [
-    ["182", "234_newuser", "abcd@gmail.com", "abc"],
-    ["234", "345_newuser", "def@gmail.com", "def"],
-    ["345", "456_newuser", "ghi@gmail.com", "ghi"],
-    ["456", "567_newuser", "jkl@gmail.com", "jkl"],
-    ["567", "678_newuser", "mno@gmail.com", "mno"],
-    ["678", "789_newuser", "pqr@gmail.com", "pqr"],
-    ["786", "890_newuser", "stu@gmail.com", "stu"],
-];
-
-
-try {
-    connection.query(q, [users], (err, result) => {
-        if (err) throw err;
-        console.log(result);
-        // console.log(result.length);
-        // console.log(result[0]);
-    })
-} catch (err) {
-    console.log(err);
-}
-connection.end();
-
-
 let getRandomUser = () => {
-    return {
-        userId: faker.string.uuid(),
-        username: faker.internet.userName(),
-        email: faker.internet.email(),
-        password: faker.internet.password()
-    };
+    return [
+        faker.string.uuid(),
+        faker.internet.userName(),
+        faker.internet.email(),
+        faker.internet.password()
+    ];
 }
+
+// // let q = "SHOW TABLES";
+// let q = "INSERT INTO user (id, username, email, password) VALUES ?";
+
+// let data = [];
+// for (let i = 0; i < 100; i++) {
+//     data.push(getRandomUser());
+//     // console.log(getRandomUser());
+// }
+
+
+
+// let users = [
+//     ["182", "234_newuser", "abcd@gmail.com", "abc"],
+//     ["234", "345_newuser", "def@gmail.com", "def"],
+//     ["345", "456_newuser", "ghi@gmail.com", "ghi"],
+//     ["456", "567_newuser", "jkl@gmail.com", "jkl"],
+//     ["567", "678_newuser", "mno@gmail.com", "mno"],
+//     ["678", "789_newuser", "pqr@gmail.com", "pqr"],
+//     ["786", "890_newuser", "stu@gmail.com", "stu"],
+// ];
+
+
+// try {
+//     connection.query(q, [data], (err, result) => {
+//         if (err) throw err;
+//         console.log(result);
+//         // console.log(result.length);
+//         // console.log(result[0]);
+//     })
+// } catch (err) {
+//     console.log(err);
+// }
+// connection.end();
+
+
+// let getRandomUser = () => {
+//     return {
+//         userId: faker.string.uuid(),
+//         username: faker.internet.userName(),
+//         email: faker.internet.email(),
+//         password: faker.internet.password()
+//     };
+// }
 // console.log(getRandomUser());
+
+
+
+
+
+
+app.get("/", (req, res) => {
+    res.send("Hello i am root path welcome to home page");
+    console.log("Request received")
+});
+
+
+console.dir();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+let port = 8080;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
---
+++ L45BackendNodewithSQL/tempCodeRunnerFile.js
@@ -0,0 +1,11 @@
+try {
+//     connection.query(q, [data], (err, result) => {
+//         if (err) throw err;
+//         console.log(result);
+//         // console.log(result.length);
+//         // console.log(result[0]);
+//     })
+// } catch (err) {
+//     console.log(err);
+// }
+// connection.end();
