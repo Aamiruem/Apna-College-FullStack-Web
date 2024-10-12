57e77ee9f5f4ceccdd47810bcd39db3f934683f2
Aamir@786 <aamirhussainazad786@gmail.com>
2024-10-12 10:55:04 +0530

    commit add

---
+++ L47MongoDBPart2/command/allCommand.js
@@ -0,0 +1,16 @@
+// 1. Using PowerShell:
+// To create a new empty file, you can use the New-Item cmdlet:
+
+// New-Item -Path .\next.js -ItemType File
+
+
+// 2. Using Command Prompt (CMD):
+// You can create an empty file using type nul:
+// for cmd
+// type nul > next.js
+
+// 3. Using Git Bash or WSL:
+// If you have Git Bash or Windows Subsystem for Linux (WSL) installed, you can use the touch command just like in Unix:
+
+// for git  bash or wsl
+// touch next.js
