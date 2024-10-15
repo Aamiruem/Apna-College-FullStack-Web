f8916b319e23151e6a27642d1d1c78d04cc3a663
Aamir@786 <aamirhussainazad786@gmail.com>
2024-10-15 12:14:00 +0530

    COMMIT ADD

--- L48MongoDBwithExpress/index.js
+++ L48MongoDBwithExpress/index.js
@@ -1,28 +1,29 @@
 const express = require("express");
 
 const app = express();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const path = require("path");
-const Chat = require("./models/chat.js")
-app.use("views", path.join(__dirname, "views"));
-app.use("views engine", "ejs")
+const Chat = require("./models/chat.js");
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs")
 
-app.use(express.static(path.join(__dirname, "public")));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 
 
 
-main().then(() => {
-    console.log("Connection Successful");  
-  
-})
+main()
+    .then(() => {
+        console.log("Connection Successful");
+
+    })
     .catch((err) => {
         console.log(err)
     });
 
 async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
+    await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
 }
 
 let chat1 = new Chat({
