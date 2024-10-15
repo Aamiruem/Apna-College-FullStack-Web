d217cf17dce5956f4cf592e7909914f0d5a39125
Aamir@786 <aamirhussainazad786@gmail.com>
2024-10-15 11:37:08 +0530

    commit add

--- L48MongoDBwithExpress/index.js
+++ L48MongoDBwithExpress/index.js
@@ -1,8 +1,39 @@
 const express = require("express");
 
 const app = express();
+const mongoose = require('mongoose');
+const path = require("path");
+const Chat = require("./models/chat.js")
+app.use("views", path.join(__dirname, "views"));
+app.use("views engine", "ejs")
 
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+main().then(() => {
+    console.log("Connection Successful");  
+  
+})
+    .catch((err) => {
+        console.log(err)
+    });
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
+}
+
+let chat1 = new Chat({
+    form: "neha",
+    to: "kamran",
+    message: "Hello kamran send me your exam sheet!",
+    created_at: new Date()
+});
+chat1.save().then((res) => {
+    console.log(res, "Chat saved successfully");
+});
 
 app.get("/", (req, res) => {
     res.send("Welcome to the API!");
