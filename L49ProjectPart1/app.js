50ad1700b80ac93540d9f114cd93daa77dbc002d
Aamir Hussain <aamirhussainazad786@gmail.com>
2024-10-23 12:28:12 +0530

    commit add

--- L49ProjectPart1/app.js
+++ L49ProjectPart1/app.js
@@ -3,12 +3,12 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
-// app.set("views", path.join(__dirname, "views"));
-// app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs")
 
-// app.use(express.static(path.join(__dirname, "public")));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 
 
