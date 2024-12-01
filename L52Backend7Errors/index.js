268120df74a8d78afefc1ddff87a7fef3db8055b
“Aamir <aamirhussainazad786@gmail.com>
2024-12-01 13:38:10 +0530

    commit add

--- L52Backend7Errors/index.js
+++ L52Backend7Errors/index.js
@@ -76,17 +76,41 @@ app.post("/chats", async (req, res) => {
         // console.log(err);
         next(err);
     }
- });
+});
+ 
+function asyncWrap(fn) {
+    return function (req, res, next) {
+        fn(req, res, next).catch((err) => next(err));
+    };
+}
+
+
+//NEW - async Show Route
+app.get("/chats/:id", asyncWrap(async (req, res, next) => {
     
-    
-//NEW - Show Route
-app.get("/chats/:id", async (req, res, next) => {
     let { id } = req.params;
     let chat = await Chat.findById(id);
     if (!chat) {
         throw new ExpressError(500, "CHAT not found some random error");
     }
     res.render("edit.ejs", { chat });
+}));
+
+    
+
+//NEW - Show Route
+app.get("/chats/:id", async (req, res, next) => {
+    try {
+        
+        let { id } = req.params;
+        let chat = await Chat.findById(id);
+        if (!chat) {
+            throw new ExpressError(500, "CHAT not found some random error");
+        }
+        res.render("edit.ejs", { chat });
+    } catch (err) {
+        next(err);
+    }
 });
 
 
@@ -101,9 +125,9 @@ app.get("/chats/:id/edit", async (req, res) => {
         }
 });
 
-//Update Route
-app.put("/chats/:id", async (req, res) => {
-    let { id } = req.params;
+//asyncWrap Update Route
+app.put("/chats/:id",asyncWrap(async (req, res, next) => {
+        let { id } = req.params;
     let {msg: newMsg } = req.body;
     console.log(newMsg);
     let updatedChat = await Chat.findByIdAndUpdate(id,
@@ -112,6 +136,54 @@ app.put("/chats/:id", async (req, res) => {
     );
     console.log(updatedChat);
     res.redirect("/chats");
+}));
+
+
+
+//Update Route
+app.put("/chats/:id", async (req, res, next) => {
+    try {
+        let { id } = req.params;
+    let {msg: newMsg } = req.body;
+    console.log(newMsg);
+    let updatedChat = await Chat.findByIdAndUpdate(id,
+        { msg: newMsg },
+        { runValidators: true, new: true }
+    );
+    console.log(updatedChat);
+    res.redirect("/chats");
+    } catch (err) {
+        next(err);
+    }
+});
+
+//asyncWrap Destroy Routes
+app.delete("/chats/:id", asyncWrap(async (req, res, next) => {
+        let { id } = req.params;
+        const chat = await Chat.findByIdAndDelete(id);
+        let deletedChat = await Chat.findByIdAndDelete(id);
+        console.log(`deleted: ${chat}`);
+        console.log(deletedChat);
+        res.redirect("/chats");
+}));
+
+
+//Destroy Routes
+app.delete("/chats/:id", async (req, res, next) => {
+    try {
+        let { id } = req.params;
+        const chat = await Chat.findByIdAndDelete(id);
+        let deletedChat = await Chat.findByIdAndDelete(id);
+        console.log(`deleted: ${chat}`);
+        console.log(deletedChat);
+        res.redirect("/chats");
+    } catch (err) {
+        next(err);
+        }
+});
+
+app.get("/", (req, res) => {
+    res.send("/chats");
 });
 
 // Delete Route
@@ -135,6 +207,22 @@ app.delete("/chats/:id", async (req, res) => {
 
 app.get("/", (req, res) => {
     res.send("Welcome to the API!");
+});
+
+const handleValidationErr = (err) => {
+    console.log("This was a validation error occurred! Please follow rules!");
+    console.dir(err);
+    console.dir(err.message);
+    return err;
+};
+
+app.use((err, req, res, next) => {
+    console.log(err.name);
+    if (err.name === "ValidationError") {
+        err = handleValidationErr(err);
+    }
+    next(err);
+    // res.status(404).send("Page Not Found");
 });
 
 
