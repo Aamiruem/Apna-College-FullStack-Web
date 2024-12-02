a6ffbd69f7c6ea314f380ca8f79265bec34954d3
“Aamir <aamirhussainazad786@gmail.com>
2024-12-02 12:50:30 +0530

    commit add

--- L53Project-phase1(partc)/part(c)/wanderlustAirbnb-ApnaCollege--main/wanderlustAirbnb-ApnaCollege--main/app.js
+++ L53Project-phase1(partc)/part(c)/wanderlustAirbnb-ApnaCollege--main/wanderlustAirbnb-ApnaCollege--main/app.js
@@ -84,6 +84,22 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render("error.ejs", { err });
 });
 
+
+
+
+// app.get("/testListing", async (req, res) => {
+// let sampleListing = new Listing({
+// title: "My New Villa",
+// description: "By the beach",
+// price: 1200,
+// location: "Calangute, Goa",
+// country: "India",
+// });
+// await sampleListing.save();
+// console.log("sample was saved");
+// res.send("successful testing");
+// });
+
 app.listen(port, () => {
   console.log(`App is running on port ${port}`);
 });
