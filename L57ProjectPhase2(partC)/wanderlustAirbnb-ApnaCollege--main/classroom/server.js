a499b2f88a767b4d41669dca52498381ea81b0f0
“Aamir <aamirhussainazad786@gmail.com>
2024-12-09 17:48:52 +0530

    commit add

--- L57ProjectPhase2(partC)/wanderlustAirbnb-ApnaCollege--main/classroom/server.js
+++ L57ProjectPhase2(partC)/wanderlustAirbnb-ApnaCollege--main/classroom/server.js
@@ -5,8 +5,19 @@ const posts = require(". /routes/post.js");
 const session = require("express-session");
 
 app.use(session({
-    secret: "mysupersecretstring"}));
+    secret: "mysupersecretstring", 
+    resave: false,
+    saveUninitialized: true,
+}));
 
+app.get("/reqcount", (req, res) => {
+    if(req.session.count){
+        req.session.count++;
+    } else {
+        req.session.count = 1;
+    }
+    res.send(`Request count: ${req.session.count}`);
+});
 
 app.listen(3000, () =>{
     console.log("Server is running on port 3000");
