fe2f8f1377975f7b7864d9bdcbb32302816b2d43
“Aamir <aamirhussainazad786@gmail.com>
2024-12-19 11:16:58 +0530

    Commit add

---
+++ L62ProjectPhase3(PartB)/WanderLust/Wander-Lust-main/routes/listing.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const router = express.Router();
+const wrapAsync = require("../utils/wrapAsync.js");
+const ExpressError = require("../utils/ExpressError.js");
+const {listingSchema} = require("../schema.js");
+const Listing = require("../models/listing.js");
+const {isLoggedIn} = require("../middleware.js");
+const {isOwner} = require("../middleware.js");
+const {validateListing} = require("../middleware.js");
+const listingController = require("../controllers/listings.js");
+const multer  = require('multer');
+const {storage} = require("../cloudConfig.js");
+const upload = multer({ storage });
+
+// //Index Route
+// router.get("/", wrapAsync(listingController.index));
+
+// //Show Route
+// router.get("/:id", wrapAsync(listingController.showListing));
+
+// //Create Route
+// router.post("/", isLoggedIn, upload.single("listing[image]"), validateListing, wrapAsync(listingController.createListing));
+
+router
+.route("/")
+.get( wrapAsync(listingController.index))
+.post(isLoggedIn,  upload.single("listing[image]"),validateListing, wrapAsync(listingController.createListing));
+
+//New Route
+
+router.get("/new",isLoggedIn,listingController.renderNewForm);
+
+router
+.route("/:id")
+.get(wrapAsync(listingController.showListing))
+.put(isLoggedIn, isOwner, upload.single("listing[image]"),validateListing ,wrapAsync(listingController.updateListing))
+.delete(isLoggedIn, isOwner,  wrapAsync(listingController.deleteListing));
+
+//Edit Route
+router.get("/:id/edit", isLoggedIn,  isOwner, wrapAsync(listingController.editListing));
+
+module.exports = router;
