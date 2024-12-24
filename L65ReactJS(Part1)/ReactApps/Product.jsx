268e907affa2c4d04ee8e11b20aa3272a9c8eb1b
“Aamir <aamirhussainazad786@gmail.com>
2024-12-24 12:53:45 +0530

    commit add

---
+++ L65ReactJS(Part1)/ReactApps/Product.jsx
@@ -0,0 +1,12 @@
+
+import "./Product.css";
+function Product() {
+    return (
+        <div className="product">
+            <h1>Product</h1>
+            <p>Product Description</p>
+        </div>
+    );
+}
+
+export default Product;
---
+++ L65ReactJS(Part1)/ReactApps/ProductTab.jsx
@@ -0,0 +1,15 @@
+import Product from "./Product.jsx";
+
+function ProductTab() {
+  return (
+    <div className="productTab">
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+    </div>
+  );
+}
+
+export default ProductTab;
