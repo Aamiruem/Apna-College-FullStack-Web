1a37d4abcbe66d496da25beb80979cecf5d9310e
“Aamir <aamirhussainazad786@gmail.com>
2024-12-26 14:17:47 +0530

    commit add

--- L66ReactPart2/ReactApp/src/Product.jsx
+++ L66ReactPart2/ReactApp/src/Product.jsx
@@ -10,7 +10,8 @@ function Product(title, price, features) {
             <h5>Price: {price}</h5>
             <h1>Product</h1>
             <p>Product Description</p>
-            <p>{features} </p>
+            <p>{features.map((feature) => <li>{feature}</li>)} </p>
+
         </div>
     );
 }
