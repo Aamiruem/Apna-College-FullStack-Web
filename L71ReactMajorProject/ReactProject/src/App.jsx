977a20242399fe470270aaf01a2bfee95da1403b
“Aamir <aamirhussainazad786@gmail.com>
2025-01-06 10:27:52 +0530

    commit add

---
+++ L71ReactMajorProject/ReactProject/src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import Button from '@mui/material/Button';
+
+function App() {
+ 
+
+  return (
+    <>
+      <div>
+        <h1>React Project</h1>
+        <h2>Material UI Demo</h2>
+        <Button variant="contained" color="primary">
+          Click Me!
+        </Button>
+      </div>
+    </>
+  )
+}
+
+export default App
