41247e8238e8c885c83a9313b04ccdf53b573246
“Aamir <aamirhussainazad786@gmail.com>
2025-01-09 12:31:07 +0530

    commit add

---
+++ L73ReduxAndReduce/ReduxApp/src/features/todo/todoSlice.js
@@ -0,0 +1,42 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+    todos: [{ id: "abc", task: "demo-task", isDone: false }],
+};
+ export const todoSlice = createSlice({
+    name: "todo",
+    initialState,
+    reducers: {
+        addTodo: (state, action) => {
+            state.todos.push(action.payload);
+            const newTodo = {
+                id: nanoid,
+                task: action.payload,
+                isDone: false,
+            }
+            state.todos.push(newTodo);
+        },
+        deleteTodo: (state, action) => {
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+        },
+        markAsDone: (state, action) => {
+            state.todos = state.todos.map((todo) => {
+                if (todo.id === action.payload) {
+                    return { ...todo, isDone: true };
+                }
+                return todo;
+            });
+        },
+        toggleTodo: (state, action) => {
+            state.todos = state.todos.map((todo) => {
+                if (todo.id === action.payload) {
+                    return { ...todo, isDone: !todo.isDone };
+                }
+                return todo;
+            });
+        },
+    }
+});
+
+export const { addTodo, deleteTodo, markAsDone, toggleTodo } = todoSlice.actions;
+export default todoSlice.reducer;
