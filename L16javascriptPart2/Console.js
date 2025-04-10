94ee733d3901534486f9c420c2734be1a98c8b1d
“Aamir <aamirhussainazad786@gmail.com>
2025-04-10 11:58:39 +0530

    commit add

---
+++ L16javascriptPart2/Console.js
@@ -0,0 +1,19 @@
+// JavaScript Code Demo
+
+// 1. Simple log statement
+console.log("Hello World from Apna College!");
+
+// 2. Variable declaration
+let num = 123;
+
+// 3. Logging a string literal
+console.log("num");  // Outputs the string "num"
+
+// 4. Logging the variable value
+console.log(num);    // Outputs 123
+
+// 5. Logging an expression result
+console.log(1 + 2);  // Outputs 3
+
+// 6. Logging multiple values (corrected from the malformed version)
+console.log("Hello World", num, (1 + 5));  // Outputs: Hello World 123 6
