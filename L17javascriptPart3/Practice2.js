79f656ff218ae114f8fb4e09e5f0c522e223f7f4
“Aamir <aamirhussainazad786@gmail.com>
2025-04-19 23:19:32 +0530

    commit add

--- L17javascriptPart3/Practice2.js
+++ L17javascriptPart3/Practice2.js
@@ -6,3 +6,18 @@ console.log(Months.concat(final));
 console.log(Months.splice(0, 2, "july", "june"));
 
 console.log(Months);
+
+
+
+
+let months = ['jan', 'june', 'july', 'march', 'august'];
+months.splice(0, 1); // removes 'jan'
+[months[0], months[1]] = [months[1], months[0]]; // swaps june <-> july
+console.log(months); // ['july', 'june', 'march', 'august']
+
+
+let arr = ['c', 'c++', 'html', 'javascript', 'python', 'java', 'c#', 'sql'];
+
+let reversed = [...arr].reverse();
+let index = reversed.indexOf("javascript");
+console.log(index); // 4
