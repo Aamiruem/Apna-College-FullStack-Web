ca97de79e7f3d4ab3ad8e20eec67997bb6c1e26c
“Aamir <aamirhussainazad786@gmail.com>
2025-04-29 13:43:51 +0530

    commit add

--- L18JvascriptPart4/forLoop.js
+++ L18JvascriptPart4/forLoop.js
@@ -12,3 +12,14 @@ for(let i = 1; i<=5; i++){
 for (let i = 0; i < 5; i++) {
     console.log("The number is " + i);
 }
+
+console.log("1");
+console.log("2");
+console.log("3");
+console.log("4");
+console.log("5");
+console.log("6");
+
+for(let i = 10; i>=1; i--){
+    console.log(i, "hello world");
+}
