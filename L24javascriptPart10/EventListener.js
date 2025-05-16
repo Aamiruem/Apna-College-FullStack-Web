10df2f3f92007535baf7ef2c138aad4df7414286
Aamir@786 <aamirhussainazad786@gmail.com>
2025-05-16 20:31:19 +0530

    commit add

---
+++ L24javascriptPart10/EventListener.js
@@ -0,0 +1,42 @@
+// let btn = document.querySelector("button");
+// console.dir(btn);
+// btn.onclick = function (event) {
+//     console.log(event);
+//     console.log("button was clicked");
+//     console.log(event.clientX);
+//     alert("button was clicked");
+// };
+
+// for(btn of btns) {
+//     console.dir(btn);
+//     btn.onclick = function (event) {
+//         console.log(event);
+//         console.log("button was clicked");
+//         console.log(event.clientX);
+//         alert("button was clicked");
+//     };
+// }
+
+
+
+
+//this is a button event listener example
+let btns = document.querySelectorAll("button");
+console.dir(btns);
+btns.addEventListener("click, sayHello");
+
+function sayHello() {
+    console.log("Hello");
+}
+
+function sayName() {
+    console.log("Hello");
+}
+
+
+btn.addEventListener("click", function (event) {
+    console.log(event);
+    console.log("button was clicked");
+    console.log(event.clientX);
+    alert("button was clicked");
+});
