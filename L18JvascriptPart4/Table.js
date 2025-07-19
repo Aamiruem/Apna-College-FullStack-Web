2c5ba35fa2cda0739c464638b70d3447b9a0fe3f
Aamir@786 <aamirhussainazad786@gmail.com>
2025-07-19 20:23:00 +0530

    commit add

--- L18JvascriptPart4/Table.js
+++ L18JvascriptPart4/Table.js
@@ -1,5 +1,5 @@
 let n = prompt("Write your number: ");
-n = parseInt(n);
+n = parseInt(n);// convert string to number
 
 for(let i = 1; i <= 10; i++) {
     console.log(n + " * " + i + " = " + (n * i));
